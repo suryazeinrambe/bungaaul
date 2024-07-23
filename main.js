46a992068dc0b06cfc739f02441776951c209656
Surya Zein Rambe <suryazein2002@gmail.com>
2024-07-23 23:15:25 +0700

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  document.body.classList.remove("container");
 };
+
+var typed = new Typed(".text", {
+  strings: ["Ani Lia 😋😋", "Ciya😋", "Aul", "Sebaiknya Uninstal ML😓", "Lia sayang ken mwah 😘"],
+  smartBackSpace: true,
+  typeSpeed: 100,
+  loop: true,
+});
